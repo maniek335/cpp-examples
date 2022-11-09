f5d0b2317ff82f83639dfcf63b2f4526085b05cf
Kubisz Mariusz <kontakt@kubiszmariusz.pl>
2022-11-09 22:05:41 +0100

    ⏬ Order of definitions
    -> The order of definitions has been changed:
    1. variables
    2. static variables
    3. constructor
    4. deconstructor
    5. method
    6. static method

--- include/Person.h
+++ include/Person.h
@@ -17,14 +17,17 @@ class Person {
   Gender gender;
   string city;
 
-  static void info();
   static int last_id;
   static int object_count;
-  void introduce();
+
   Person(string firstname = "",
          string lastname = "",
          string birthdate = "",
          Gender gender = Gender::Undefined,
          string city = "");
   ~Person();
+
+  void introduce();
+
+  static void info();
 };
